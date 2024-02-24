1891d95698bb148eff9f86a148bf96fe7bd26b19
DIPURANJAN SETHY <dipuranjansethy1432@gmail.com>
2024-02-24 22:11:19 +0530

    dbchange

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://0.0.0.0:27017/MealpieRegistration").then(()=>{
+mongoose.connect("mongodb+srv://dipuranjansethy1432:mongopassword@cluster0.4f5sap9.mongodb.net/mealpie?retryWrites=true&w=majority").then(()=>{
     console.log("connnection established")
 }).catch((err)=>{
     console.log(err);
